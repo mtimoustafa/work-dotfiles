8f41d33fe9aba04858cb46df1254cf4929b3f851
Mohamed Moustafa <mohamed.moustafa@reaktor.com>
2025-10-31 13:37:31 +0100

    Add navigation keymappings for vim gitsigns

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -1,15 +1,21 @@
 -- Git gutter+ features within Neovim
 return {
-  "lewis6991/gitsigns.nvim",
+  'lewis6991/gitsigns.nvim',
   config = function()
-    require("codewindow").setup({
+    require('gitsigns').setup({
       signs_staged_enable = true,
-    })
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
 
-    vim.keymap.set("n", "<Leader>d", ":Gitsigns toggle_word_diff<CR>")
-    vim.keymap.set("n", "<Leader>D", ":Gitsigns diffthis<CR>")
-    vim.keymap.set("n", "<Leader>b", ":Gitsigns blame_line<CR>")
-    vim.keymap.set("n", "<Leader>B", ":Gitsigns blame<CR>")
+        vim.keymap.set('n', ']c', function() gitsigns.nav_hunk('next') end, { desc = 'GitSigns go to next hunk' })
+        vim.keymap.set('n', '[c', function() gitsigns.nav_hunk('prev') end, { desc = 'GitSigns go to previous hunk' })
+
+        vim.keymap.set('n', '<Leader>d', ':Gitsigns toggle_word_diff<CR>')
+        vim.keymap.set('n', '<Leader>D', ':Gitsigns diffthis<CR>')
+        vim.keymap.set('n', '<Leader>b', ':Gitsigns blame_line<CR>', { desc = 'Gitsigns blame line' })
+        vim.keymap.set('n', '<Leader>B', ':Gitsigns blame<CR>', { desc = 'Gitsigns blame' })
+      end,
+    })
   end,
 }
 
