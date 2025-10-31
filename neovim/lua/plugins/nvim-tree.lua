01ad77bc15da48d4f1adf5d0ad68045b147d4380
Mohamed Moustafa <mohamed.moustafa@reaktor.com>
2025-10-31 16:30:32 +0100

    Update configs for nvim-tree

--- neovim/lua/plugins/nvim-tree.lua
+++ neovim/lua/plugins/nvim-tree.lua
@@ -1,15 +1,28 @@
 -- File explorer
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   lazy = false,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    vim.keymap.set("n", "<Leader>f", ":NvimTreeToggle<CR>")
-    vim.keymap.set("n", "<Leader>F", ":NvimTreeFindFile<CR>")
+    vim.keymap.set('n', '<Leader>f', ':NvimTreeToggle<CR>')
+    vim.keymap.set('n', '<Leader>F', ':NvimTreeFindFile<CR>')
 
-    require("nvim-tree").setup({}) -- Config goes here
+    require('nvim-tree').setup({
+      view = {
+        width = 50,
+      },
+      renderer = {
+        hidden_display = 'all',
+        indent_markers = {
+          enable = true,
+        },
+      },
+      filters = {
+        git_ignored = false,
+      },
+    }) -- Config goes here
   end,
 }
